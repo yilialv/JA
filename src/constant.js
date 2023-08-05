00440d93123d1a72caf2c9e6af58c651b08a5cd7
jiaying.lv <jiaying.lv@qunar.com>
2023-08-05 14:42:26 +0800

    隐藏鉴权信息

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,4 @@
-export const APPID = 36915985
-export const APPKEY = "O5vbXSd4OyLGYMaKbGVGtVip"
+export const APPID = 
+export const APPKEY = ""
 export const DEV_PID = 15372
 export const URI = "ws://vop.baidu.com/realtime_asr"
